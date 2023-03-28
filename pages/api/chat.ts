a1ada12ca54b3bab024fa0b952a6ce4e60652f04
Albert <lb13810398408@gmail.com>
2023-03-28 12:26:12 +0900

    feat: Integrate Pinecone search engine into chatbot functionality.
    - Add Pinecone database integration to chat API
    - Upgrade project dependencies, including React and eslint
    - Lower the temperature of OpenAIStream API and improve debugging in server utility module

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -5,14 +5,137 @@ import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
 import { init, Tiktoken } from '@dqbd/tiktoken/lite/init';
 // @ts-expect-error
 import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
+import { PineconeClient } from "@pinecone-database/pinecone";
 
 export const config = {
   runtime: 'edge',
 };
 
+const reset = "\x1b[0m";
+const red = "\x1b[31m";
+const green = "\x1b[32m";
+const yellow = "\x1b[33m";
+const blue = "\x1b[34m";
+const magenta = "\x1b[35m";
+const cyan = "\x1b[36m";
+
 const handler = async (req: Request): Promise<Response> => {
   try {
     const { model, messages, key, prompt } = (await req.json()) as ChatBody;
+
+    /////
+    const userMessageContent = [...messages].reverse().find((message) => message.role === "user")?.content || "";
+    
+    const res = await fetch("https://api.openai.com/v1/embeddings", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key ? key : process.env.OPENAI_API_KEY}`
+      },
+      method: "POST",
+      body: JSON.stringify({
+        model: "text-embedding-ada-002",
+        input: userMessageContent,
+      })
+    });
+
+    if (res.status !== 200) {
+      const statusText = res.statusText; 
+      throw new Error(`OpenAI API returned an error: ${statusText}`);
+    }
+    const embedding = await res.json();
+    //console.log(embedding);
+
+    const pinecone = new PineconeClient();
+    await pinecone.init({
+      environment: "us-central1-gcp",
+      apiKey: process.env.PINECONE_API_KEY || '',
+    });
+
+    //pinecone.projectName = "chatwith"
+    const index = pinecone.Index("shadow-tv");
+    const queryRequest = {
+      vector: embedding.data[0].embedding,
+      topK: 3,
+      includeValues: false,
+      includeMetadata: true,
+      namespace: "shadowtv20230326",
+    };
+    //console.log(queryRequest);
+    let queryResponse;
+    try {
+      queryResponse = await index.query({ queryRequest });
+    } catch (error) {
+      console.error("Error querying Pinecone index:", error);
+      queryResponse = null;
+    }
+
+    interface Metadata {
+      text: string;
+      bvid: string;
+      ytid: string;
+      title: string;
+    }
+    
+    if (queryResponse && queryResponse.matches) {
+      let others: string[] = [];
+      const combinedTexts = queryResponse.matches
+        .map((result) => {
+          const metadata = result.metadata as Metadata; // Cast metadata to the Metadata interface
+          return metadata?.text || '';
+        })
+        .filter((text) => text.trim().length > 0)
+        .join(" ");
+
+      console.log("用户问题:");
+      console.log(`${yellow}${userMessageContent}${reset}`);
+
+      for (const result of queryResponse.matches) {
+        const metadata = result.metadata as Metadata; // Cast metadata to the Metadata interface
+        
+        const yt = `https://www.youtube.com/watch?v=${metadata.ytid}`;
+        const bili = `https://www.bilibili.com/video/${metadata.bvid}`;
+
+        const other = `相关视频: ${metadata.title}
+        ${yt}`;
+        others.push(other);
+        // Process metadata as needed
+
+        const metadataJsonString = JSON.stringify(metadata, null, 2);
+        console.log("Metadata:");
+        //console.log(metadata);
+
+        console.log(`${green}${metadataJsonString}${reset}`);
+      }
+      //console.log("combinedTexts:", combinedTexts);
+      const uniqueArrayOthers = Array.from(new Set(others));
+
+      const template = `你扮演一个叫做"黑影儿"的人。根据段落中文字的内容回答问题。
+不要自己创造答案，不要说除了答案以外的任何内容，如果无法确定答案就回答不知道。如果段落中有错别字，请修改错别字后再使用。
+答案请返回Markdown格式。必须将参考资料的内容拼接在答案之后。
+问题: 
+"""
+${userMessageContent}
+"""
+段落:
+"""
+${combinedTexts}
+"""
+参考资料:
+"""
+${uniqueArrayOthers.join("\n")}
+"""
+第一人称答案 in Markdown:`;
+
+      const lastUserMessageIndex = messages
+        .map((message, index) => ({ message, index }))
+        .filter(({ message }) => message.role === "user")
+        .slice(-1)
+        .map(({ index }) => index)[0];
+
+      if (lastUserMessageIndex !== undefined) {
+        messages[lastUserMessageIndex].content = template;
+      }
+    }
 
     await init((imports) => WebAssembly.instantiate(wasm, imports));
     const encoding = new Tiktoken(
@@ -46,6 +169,7 @@ const handler = async (req: Request): Promise<Response> => {
 
     encoding.free();
 
+    //console.log("messagesToSend:", messagesToSend);
     const stream = await OpenAIStream(model, promptToSend, key, messagesToSend);
 
     return new Response(stream);
--- utils/server/index.ts
+++ utils/server/index.ts
@@ -6,6 +6,15 @@ import {
 } from 'eventsource-parser';
 import { OPENAI_API_HOST } from '../app/const';
 
+let logMessages: string[] = [];
+const reset = "\x1b[0m";
+const red = "\x1b[31m";
+const green = "\x1b[32m";
+const yellow = "\x1b[33m";
+const blue = "\x1b[34m";
+const magenta = "\x1b[35m";
+const cyan = "\x1b[36m";
+
 export const OpenAIStream = async (
   model: OpenAIModel,
   systemPrompt: string,
@@ -28,7 +37,7 @@ export const OpenAIStream = async (
         ...messages,
       ],
       max_tokens: 1000,
-      temperature: 1,
+      temperature: 0.2,
       stream: true,
     }),
   });
@@ -48,6 +57,11 @@ export const OpenAIStream = async (
           const data = event.data;
 
           if (data === '[DONE]') {
+            console.log("GPT回答:");
+            const answer = logMessages.join('');
+            console.log(`${magenta}${answer}${reset}`);
+            console.log("--------------------------------------------");
+            logMessages = [];
             controller.close();
             return;
           }
@@ -55,6 +69,8 @@ export const OpenAIStream = async (
           try {
             const json = JSON.parse(data);
             const text = json.choices[0].delta.content;
+            
+            logMessages.push(text);
             const queue = encoder.encode(text);
             controller.enqueue(queue);
           } catch (e) {
